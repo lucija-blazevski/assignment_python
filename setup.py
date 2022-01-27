c6826216086e82a6bd15fef14ab5637d9efb6622
lucija-blazevski <94177326+lucija-blazevski@users.noreply.github.com>
2022-01-28 00:51:09 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cheat',
+    version='0.0.1',
+    packages=['cheat'],
+    url='https://github.com/lucija-blazevski/assignment_python.git',
+    license='MIT',
+    author='Lucija',
+    author_email='blazevskilucija@gmail.com',
+    description='Answer for the last question of Python assignment'
+)
